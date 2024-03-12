d7bb1f60f0a286cf439e9757939962761f161ad1
Antyto <antonietanovoa91@gmail.com>
2024-03-12 19:09:25 -0300

    Ajustes de Smooth Scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,4 +7,21 @@ $(document).ready(function () {
     });
     /*ToolTip para imagenes el apartado de "Destacados". Al pasar el puntero por sobre las imagenes de las card, estas muestras el lugar al que pertenecen las fotografías*/
     $('[data-toggle="tooltip"]').tooltip();
+
+    /*Smooth Scroll para ir a los diferentes contenidos, desde la barra de navegación*/
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+
 });
