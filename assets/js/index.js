d4f940ad8f34055694c11fc9ace53d2d3c5aaf71
Antyto <antonietanovoa91@gmail.com>
2024-03-12 18:26:27 -0300

    HTML, CSS y JavaScript documentados e indentado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){
-    $(".nav-link").on("click",function(){  
+$(document).ready(function () {
+    /*JS con función "onClick" para links en navbar, cambia de color al hacer click.*/
+    $(".nav-link").on("click", function () {
         $(this).css({
-        "color": "hotpink",
+            "color": "hotpink",
         });
-    });  
+    });
+    /*ToolTip para imagenes el apartado de "Destacados". Al pasar el puntero por sobre las imagenes de las card, estas muestras el lugar al que pertenecen las fotografías*/
     $('[data-toggle="tooltip"]').tooltip();
 });
-   
